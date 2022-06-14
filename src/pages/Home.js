57ba61fdf8f94fe62920a9adabbf79b434b8c6ba
Manohar4 <manoahrmanu4444@gmail.com>
2022-06-14 19:46:56 +0530

    Updated Target Blank error

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -29,14 +29,14 @@ export default function Home() {
                 <div className='intro'>
                     <p style={{fontSize:'18px',fontWeight:'700',color:'#fff'}}>Manohar Manu</p>
                     <p >Hey, I'm Manohar. I'm a User Experience designer based out in Bangalore, India.</p>
-                    <p >I'm currently working in <a className='link' target='_blank' href='http://agilesolutions.com/'>Agile Solutions</a> brazil based enterprise company as Senior User Experience Design. Most of my  previous work reflects with B2B Solutions in the area of Manufacturing, Fintech and many other sectors.</p>
+                    <p >I'm currently working in <a className='link' target='_blank' rel="noreferrer" href='http://agilesolutions.com/'>Agile Solutions</a> brazil based enterprise company as Senior User Experience Design. Most of my  previous work reflects with B2B Solutions in the area of Manufacturing, Fintech and many other sectors.</p>
                     <p >  I'm a self Learned Design with an engineering background</p>
                 </div>
                 <div className='pastExpereince'>
                     <h6 className='subTitle'>Past Expereince</h6>
                     <div className='experience-row'>
                         <div className='expereince-lineitems'>
-                            <a className='link' target='_blank' href='http://agilesolutions.com/' >Agile Solutions (Brazil/India) </a>
+                            <a className='link' target='_blank' rel="noreferrer" href='http://agilesolutions.com/' >Agile Solutions (Brazil/India) </a>
                             <div style={{flexGrow:1,borderBottom:'dotted 2px #ffffff60'}}></div>
                             <div className="desgPeriod">
                                 <h4 style={{textAlign:'right'}} >Sr. Designer</h4>
@@ -46,7 +46,7 @@ export default function Home() {
 
                         </div>
                         <div className='expereince-lineitems'>
-                            <a className='link' target='_blank' href='http://agilesolutions.com/' >Agile Solutions (Brazil) </a>
+                            <a className='link' target='_blank' rel="noreferrer" href='http://agilesolutions.com/' >Agile Solutions (Brazil) </a>
                             <div style={{flexGrow:1,borderBottom:'dotted 2px #ffffff60'}}></div>
                             <div className="desgPeriod">
                                 <h4 style={{textAlign:'right'}} >Designer</h4>
@@ -55,7 +55,7 @@ export default function Home() {
                         
                         </div>
                         <div className='expereince-lineitems' >
-                            <a className='link' target='_blank' href='https://incture.com/' >Incture (Bngl, India)</a>
+                            <a className='link' target='_blank' rel="noreferrer" href='https://incture.com/' >Incture (Bngl, India)</a>
                             <div style={{flexGrow:1,borderBottom:'dotted 2px #ffffff60'}}></div>
                             <div className="desgPeriod">
                                 <h4 style={{textAlign:'right'}} >Designer</h4>
@@ -64,7 +64,7 @@ export default function Home() {
                             
                         </div>
                         <div className='expereince-lineitems'>
-                            <a className='link' target='_blank' href='https://incture.com/' >Incture (Bngl, India)</a>
+                            <a className='link' target='_blank' rel="noreferrer" href='https://incture.com/' >Incture (Bngl, India)</a>
                             <div style={{flexGrow:1,borderBottom:'dotted 2px #ffffff60'}}></div>
                             
                             <div className="desgPeriod" >
@@ -112,23 +112,23 @@ export default function Home() {
                         <div className='social-lineitems'>
                             <h4 >Dribbble </h4>
                             <div style={{flexGrow:1,borderBottom:'dotted 2px #ffffff60'}}></div>
-                            <a className='link' target='_blank' href='https://dribbble.com/manoharmanu'>Follow</a>
+                            <a className='link' target='_blank' rel="noreferrer" href='https://dribbble.com/manoharmanu'>Follow</a>
 
                         </div>
                         <div className='social-lineitems'>
                         <h4 >Behance </h4>
                            <div style={{flexGrow:1,borderBottom:'dotted 2px #ffffff60'}}></div>
-                            <a className='link' target='_blank' href='https://www.behance.net/manoharmande31'>Follow</a>
+                            <a className='link' target='_blank' rel="noreferrer" href='https://www.behance.net/manoharmande31'>Follow</a>
                         </div>
                         <div className='social-lineitems' >
                             <h4 >Twitter </h4>
                             <div style={{flexGrow:1,borderBottom:'dotted 2px #ffffff60'}}></div>
-                            <a target='_blank' href="https://twitter.com/manoharmanu4444?ref_src=twsrc%5Etfw" className="twitter-follow-button link" data-show-count="false">Follow</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+                            <a target='_blank' rel="noreferrer" href="https://twitter.com/manoharmanu4444?ref_src=twsrc%5Etfw" className="twitter-follow-button link" data-show-count="false">Follow</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
                         </div>
                         <div className='social-lineitems'>
                             <h4 >Linkedin </h4>
                             <div style={{flexGrow:1,borderBottom:'dotted 2px #ffffff60'}}></div>
-                            <a target='_blank' className='link' href='https://www.linkedin.com/in/manoharmanu4444/'>Connect</a>
+                            <a target='_blank' rel="noreferrer" className='link' href='https://www.linkedin.com/in/manoharmanu4444/'>Connect</a>
                         </div>
                     </div>
                 </div>
