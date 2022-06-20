db08fe4e285116662b8e38d40f591445c6269b3a
Manohar4 <manoahrmanu4444@gmail.com>
2022-06-20 14:12:05 +0530

    added thanks for visting section

--- src/pages/OtherActivities.js
+++ src/pages/OtherActivities.js
@@ -9,8 +9,28 @@ import XD from "./../assets/tools/XD.png"
 import Adobe_CC from "./../assets/tools/Adobe CC.png"
 import Header from '../components/Header'
 import Testimonials from '../components/Testimonials'
+import * as FiIcons from 'react-icons/fi'
+
+
+import Animation_1 from './../assets/Animations/1.mp4'
+import Animation_2 from './../assets/Animations/2.mp4'
+import Animation_3 from './../assets/Animations/3.mp4'
+import Animation_4 from './../assets/Animations/4.mp4'
+import Animation_5 from './../assets/Animations/5.mp4'
+import Animation_6 from './../assets/Animations/6.mp4'
+import Animation_7 from './../assets/Animations/7.mp4'
+import Animation_8 from './../assets/Animations/8.mp4'
+import Animation_9 from './../assets/Animations/9.mp4'
+
+import Animation_10 from './../assets/Animations/10.mp4'
+import Animation_11 from './../assets/Animations/11.mp4'
+import Animation_12 from './../assets/Animations/12.mp4'
+import Animation_13 from './../assets/Animations/13.mp4'
 
  function OtherActivities() {
+  window.currentAnimoji = 9;
+  window.previousArray = [];
+  window.startLoop = true;
 
   const back = false;
   useEffect(()=>{
@@ -21,6 +41,65 @@ import Testimonials from '../components/Testimonials'
 },
  // eslint-disable-next-line
 [])
+
+const onAnimationClick =()=>{
+
+  setTimeout(function (event) {
+    const animContainer = document.getElementsByClassName('animationGroup')[0];
+    var currentVideo = animContainer.children[window.currentAnimoji -
+        1];
+    currentVideo.style.display = "none";
+    const items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+    window.currentAnimoji = random_item(items);
+    animContainer.children[window.currentAnimoji - 1].style.display =
+        "block";
+    animContainer.children[window.currentAnimoji - 1].currentTime = 0;
+    animContainer.children[window.currentAnimoji - 1].play();
+}, 1000)
+
+}
+
+const random_item =(items) =>{
+  window.globalArray = items;
+  if (window.previousArray.length === window.globalArray.length) {
+      var lastValueLoop = window.previousArray[9];
+      window.previousArray = [];
+      window.previousArray.push(lastValueLoop);
+      window.lastLoopFlag = true;
+  }
+  window.resultedArray = arr_diff(window.previousArray, window.globalArray);
+  // var random = Math.floor((Math.random() * 10) + 1);
+  var nextNumber = window.resultedArray[Math.floor(Math.random() * window.resultedArray
+      .length)];
+  window.previousArray.push(nextNumber);
+
+  if (window.lastLoopFlag) {
+      window.previousArray.splice(0, 1);
+      window.lastLoopFlag = false;
+  }
+  return items[nextNumber - 1];
+  //return items[Math.floor(Math.random() * items.length)];
+}
+
+const arr_diff =(a1,a2) =>{
+
+  var a = [];
+
+  for (var i = 0; i <= a2.length - 1; i++) {
+      var valueCheck = true;
+      for (var j = 0; j <= a1.length - 1; j++) {
+          if (a1[j] === a2[i]) {
+              valueCheck = false;
+          }
+      }
+      if (valueCheck) {
+          a.push(a2[i]);
+      }
+  }
+  return a;
+}
+
+  
 
 
 
@@ -69,12 +148,74 @@ import Testimonials from '../components/Testimonials'
                         </div>
                     </div>
                 </div>
-                
-            
+
+               
             
             <Testimonials></Testimonials>
 
+            <div style={{display:"flex",borderRadius:"1rem",backgroundColor:"#000000"}}>
 
+<div className='animationGroup' style={{display:"flex",flex:"1",textAlign: "center",cursor: "pointer"}}>
+
+    <video style={{display: "none",borderRadius:"1rem"}} class="animoji" width="100%" height="100%" autoplay="false"
+        muted="">
+        <source id="animojiSrcId" src={Animation_1} type="video/mp4"/>
+    </video>
+    <video style={{display: "none",borderRadius:"1rem"}} class="animoji" width="100%" height="100%" autoplay="false"
+        muted="">
+        <source id="animojiSrcId" src={Animation_2} type="video/mp4"/>
+    </video>
+    <video style={{display: "none",borderRadius:"1rem"}} class="animoji" width="100%" height="100%" autoplay="false"
+        muted="">
+        <source id="animojiSrcId" src={Animation_3} type="video/mp4"/>
+    </video>
+    <video style={{display: "none",borderRadius:"1rem"}} class="animoji" width="100%" height="100%" autoplay="false"
+        muted="">
+        <source id="animojiSrcId" src={Animation_4} type="video/mp4"/>
+    </video>
+    <video style={{display: "none",borderRadius:"1rem"}} class="animoji" width="100%" height="100%" autoplay="false"
+        muted="">
+        <source id="animojiSrcId" src={Animation_5} type="video/mp4"/>
+    </video>
+    <video style={{display: "none",borderRadius:"1rem"}} class="animoji" width="100%" height="100%" autoplay="false"
+        muted="">
+        <source id="animojiSrcId" src={Animation_6} type="video/mp4"/>
+    </video>
+    <video style={{display: "none",borderRadius:"1rem"}} class="animoji" width="100%" height="100%" autoplay="false"
+        muted="">
+        <source id="animojiSrcId" src={Animation_7} type="video/mp4"/>
+    </video>
+    <video style={{display: "none",borderRadius:"1rem"}} class="animoji" width="100%" height="100%" autoplay="false"
+        muted="">
+        <source id="animojiSrcId" src={Animation_8} type="video/mp4"/>
+    </video>
+    <video style={{borderRadius:"1rem"}} class="animoji" width="100%" height="100%" autoplay="false"
+        muted="">
+        <source style={{display:"none"}} id="animojiSrcId" src={Animation_8} type="video/mp4"/>
+    </video>
+    <video style={{display: "none",borderRadius:"1rem"}} class="animoji" width="100%" height="100%" autoplay="false"
+        muted="">
+        <source id="animojiSrcId" src={Animation_10} type="video/mp4"/>
+    </video>
+    <video style={{display: "none",borderRadius:"1rem"}} class="animoji" width="100%" height="100%" autoplay="false"
+        muted="">
+        <source id="animojiSrcId" src={Animation_11} type="video/mp4"/>
+    </video>
+    <video style={{display: "none",borderRadius:"1rem"}} class="animoji" width="100%" height="100%" autoplay="false"
+        muted="">
+        <source id="animojiSrcId" src={Animation_12} type="video/mp4"/>
+    </video>
+    <video style={{display: "none",borderRadius:"1rem"}} class="animoji" width="100%" height="100%" autoplay="false"
+        muted="">
+        <source id="animojiSrcId" src={Animation_13} type="video/mp4"/>
+    </video>
+</div>
+
+<div style={{display:"flex", flexDirection:"column",flex:"1",justifyContent:"center",textAlign:"center"}}>
+  <h2 > Thanks for visiting</h2>
+  <button onClick={()=> onAnimationClick()} className='backButton' style={{width:"auto",alignSelf:"center"}}> <FiIcons.FiThumbsUp className='backIcon'/><p>Click Me</p></button>
+</div>
+            </div>
             
             </div>
         </div>
