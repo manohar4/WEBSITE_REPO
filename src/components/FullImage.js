01af12ecfde8cbc9b73c1b3282c140bc5a18ca35
Manohar4 <manoahrmanu4444@gmail.com>
2022-06-27 13:04:56 +0530

    Full Image View Added

--- src/components/FullImage.js
+++ src/components/FullImage.js
@@ -9,7 +9,7 @@ const FullImage = (props)=>{
     return (
         <div  className="popup-box"> 
                 <FiIcons.FiX style={{position: "absolute",top: "1rem",right: "1rem",color:'white',fontSize:"32px"}} className="closeIcon" onClick={props.handleClose}/>
-                <img style={{margin: "auto",borderRadius:"0.5rem",maxHeight:"80%"}} src={props.imgSrc}></img>
+                <img style={{margin: "auto",borderRadius:"0.5rem",maxHeight:"80%"}} src={props.imgSrc} alt="carouselImages"></img>
             </div>
       )
 
